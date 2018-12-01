a169cab178397595e0d8a6a5a20ff5a83aa778ab
Your NameDavidKarapetyan <you@example.com>
2018-12-01 17:29:37 +0400

    add all files

---
+++ public/Gishatich.js
@@ -0,0 +1,103 @@
+
+class gishatich {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.energy = 12;
+        this.index = index;
+        this.directions = [];
+        this.acted = false;
+        this.multiply = 0;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push(this.directions[i]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+        var newCord = random(this.chooseCell(0));
+        if (this.acted == false) {
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+                this.acted = true;
+                this.energy--;
+                if (this.energy <= 0) {
+                    this.die();
+                }
+            }
+        }
+    }
+    mul() {
+        this.multiply++;
+        var newCord = random(this.chooseCell(0));
+
+        if (newCord && this.multiply >= 8) {
+            var newX = newCord[0];
+            var newY = newCord[1];
+
+            matrix[newY][newX] = new gishatich(newX, newY, 3);
+            this.multiply = 0;
+        }
+    }
+    eat() {
+        var newCord = random(this.chooseCell(2));
+        if (this.acted == false) {
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+
+                this.energy++;
+                this.acted = true;
+                if (this.energy >= 15) {
+                    this.mul();
+                }
+            }
+            else {
+                this.move();
+            }
+
+        }
+
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+    }
+}
---
+++ public/Sun.js
@@ -0,0 +1,124 @@
+
+
+
+class Sun {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.energy = 11;
+        this.index = index;
+        this.directions = [];
+        this.acted = false;
+        this.multiply = 0;
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(num) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push(this.directions[i]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    move() {
+        var newCord = random(this.chooseCell(0));
+        if (this.acted == false) {
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+                this.acted = true;
+                this.energy--;
+                if (this.energy <= 0) {
+                    this.die();
+                }
+            }
+        }
+    }
+    mul() {
+        this.multiply++;
+        var newCord = random(this.chooseCell(0));
+
+        if (newCord && this.multiply >= 8) {
+            var newX = newCord[0];
+            var newY = newCord[1];
+
+            matrix[newY][newX] = new Sun(newX, newY, 4);
+            this.multiply = 0;
+        }
+    }
+    eat() {
+        if (this.acted == false) {
+
+            var newCord = random(this.chooseCell(3));
+
+            if (newCord) {
+                var newX = newCord[0];
+                var newY = newCord[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+
+                this.energy++;
+                this.acted = true;
+                if (this.energy >= 10) {
+                    this.mul();
+                }
+            }
+            else {
+                var newCord1 = random(this.chooseCell(1));
+
+                if (newCord1) {
+                    var newX = newCord1[0];
+                    var newY = newCord1[1];
+
+                    matrix[newY][newX] = matrix[this.y][this.x];
+                    matrix[this.y][this.x] = 0;
+
+                    this.x = newX;
+                    this.y = newY;
+
+                    this.energy++;
+                    this.acted = true;
+                    if (this.energy >= 10) {
+                        this.mul();
+                    }
+                }
+                else {
+                    this.move();
+                }
+            }
+        }
+    }
+    die() {
+        matrix[this.y][this.x] = 0;
+    }
+}
